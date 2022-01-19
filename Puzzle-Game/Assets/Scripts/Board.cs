8312176f717fdbc93b88b7c58280b38385adbe0b
sheikhjaber0424 <55077645+sheikhjaber0424@users.noreply.github.com>
2022-01-19 23:16:58 +0800

    Gem swipe done

--- Puzzle-Game/Assets/Scripts/Board.cs
+++ Puzzle-Game/Assets/Scripts/Board.cs
@@ -12,6 +12,8 @@ public class Board : MonoBehaviour
     public Gem[] gems;
 
     public Gem[,] allGems;
+
+    public float gemSpeed;
     // Start is called before the first frame update
     void Start()
     {
